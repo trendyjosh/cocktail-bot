a586f88b2677b0ec2ec9e559ef4fd28fdbfe13a2
trendyjosh <trendyjosh@gmail.com>
2024-03-16 11:14:07 +0000

    Set axios defaults

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,14 @@ import { SapphireClient, container } from "@sapphire/framework";
 import { GatewayIntentBits } from "discord.js";
 import config from "./config.json";
 import { Controller } from "./controllers/Controller";
+import axios from "axios";
 
 // Create the controller object
 container.controller = new Controller();
+
+// Set axios defaults
+axios.defaults.headers.common["X-Api-Key"] = config.apiNinjaToken;
+axios.defaults.baseURL = config.apiNinjaEndpoint;
 
 // Initiate the Sapphire client
 const client = new SapphireClient({
